01914f83bd2ecb190253eedc030d02522fd7b914
satyaprakash3636 <satyaprakash3636@gmail.com>
2020-08-22 19:46:46 +0530

    Changes to send mail to multiple recipent & added getpass

--- keepass.py
+++ keepass.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 import fileinput
+import getpass
 from pykeepass import PyKeePass
 import json
 from json2html import *
@@ -15,10 +16,9 @@ import smtplib
 parser = argparse.ArgumentParser(
     description='Search for string including partial word in Keepass Database')
 parser.add_argument(
-    'filename', help='file which contains partial (or complete) strings to search')
+    'inputfile', help='file which contains partial (or complete) strings to search')
 parser.add_argument('keepassdb', help='Path to Keepass Database file')
-parser.add_argument('password', help='Password of Keepass Database file')
-parser.add_argument('emailto', help='TO Email ID')
+parser.add_argument('emailto', help='TO Email ID(Comma separated)')
 parser.add_argument('--subject', '-s',
                     help='Email subject for which scope is being checked')
 
@@ -45,16 +45,17 @@ def send_email(toaddr, FileName):
 
     server = smtplib.SMTP('smtp-eu.sanofi.com', 25)
     text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
+    server.sendmail(fromaddr, toaddr.split(','), text)
     server.quit()
 
 try:
-    f = open(args.filename)
-    kp = PyKeePass(args.keepassdb, password=args.password)
+    dbpass = getpass.getpass(prompt='Enter the Keepass Password: ')
+    f = open(args.inputfile)
+    kp = PyKeePass(args.keepassdb, password=dbpass)
 except FileNotFoundError as err:
     print(f"Exception : {err}")
 except Exception:
-    print(f"Exception: pykeepass.exceptions.CredentialsError")
+    print(f"Exception: pykeepass.exceptions.CredentialsError, kindly provide correct password")
 
 else:
     entry = kp.find_entries(title='.*', regex=True,
@@ -119,8 +120,9 @@ else:
 
 # Defining HTML style and writing list of server which were found in keepass DB
         with open(f'{dbname}.html', 'a') as f4:
-            f4.write('<head> <meta name="description" content="Keepass Search Result"> <meta name="author" content="Satyaprakash Prasad"> </head>')
-            f4.write("<style> body { background-color: darkslategrey; color: azure; font-size: 1.1em; } h1 { color: coral; } #intro { font-size: 1.3em; } .colorful { color: orange; } .myTable { width: 100%; text-align: center; background-color: lemonchiffon; border-collapse: collapse; } .myTable th { background-color: goldenrod; color: white; } .myTable td { padding: 2px; border: 1px solid goldenrod; color: black } .myTable th { padding: 2px; border: 1px solid goldenrod; } </style>")
+            f4.write('<!DOCTYPE html> <html> <head> <meta name="description" content="Keepass Search Result"> <meta name="author" content="Satyaprakash Prasad"> ')
+            f4.write("<style> body { background-color: darkslategrey; color: azure; font-size: 1.1em; } h1 { color: coral; } #intro { font-size: 1.3em; } .colorful { color: orange; } .myTable { width: 100%; text-align: center; background-color: lemonchiffon; border-collapse: collapse; } .myTable th { background-color: goldenrod; color: white; } .myTable td { padding: 2px; border: 1px solid goldenrod; color: black } .myTable th { padding: 2px; border: 1px solid goldenrod; } </style> </head>")
+            f4.write('<body>')
             f4.write(f'<h1>{len(finallist)} servers found in {dbname} Keepass</h1>')
             if args.subject:
                 f4.write(f'<p id="intro">Email : {args.subject}</p>')
@@ -159,6 +161,7 @@ else:
             f4.write(f'<p id="intro">Below are the servers which are not in Keepass:</p>')
             notfound = list(set(inputnodupsnoblank) - set(finallist))
             f4.write(json2html.convert(json=notfound))
+            f4.write('</body> </html>')
         #print(f'Input data : {inputnodupsnoblank}')
         #print(f'Final list: {finallist}')
         with open(f'{dbname}.html', 'rt') as f4:
@@ -175,7 +178,7 @@ else:
     else:
         with open(f'{dbname}.html', 'a') as f4:
             f4.write('<head> <meta name="description" content="Keepass Search Result"> <meta name="author" content="Satyaprakash Prasad"> </head>')
-            f4.write("<style> body { background-color: darkslategrey; color: azure; font-size: 1.1em; } h1 { color: coral; } #intro { font-size: 1.3em; } .colorful { color: orange; } .myTable { width: 100%; text-align: center; background-color: lemonchiffon; border-collapse: collapse; } .myTable th { background-color: goldenrod; color: white; } .myTable td { padding: 2px; border: 1px solid goldenrod; color: black } .myTable th { padding: 2px; border: 1px solid goldenrod; } </style>")
+            f4.write("<style> body { background-color: darkslategrey; color: Silver; font-size: 1.1em; } h1 { color: coral; } #intro { font-size: 1.3em; } .colorful { color: orange; } .myTable { width: 100%; text-align: center; background-color: lemonchiffon; border-collapse: collapse; } .myTable th { background-color: goldenrod; color: white; } .myTable td { padding: 2px; border: 1px solid goldenrod; color: black } .myTable th { padding: 2px; border: 1px solid goldenrod; } </style>")
             f4.write(f'<h1>{len(finallistdups)} servers found in {dbname} Keepass</h1>')
             if args.subject:
                 f4.write(f'<p id="intro">Email : {args.subject}</p>')
